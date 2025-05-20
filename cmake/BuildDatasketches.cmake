3df1a20fa601aaac5b667669bf98534655e0e363
ortur <orturb@yandex.ru>
2025-05-20 18:07:00 +0300

    Enhance: better configuring

--- cmake/BuildDatasketches.cmake
+++ cmake/BuildDatasketches.cmake
@@ -5,27 +5,33 @@ set(DATASKETCHES_BUILD_DIR "${CMAKE_CURRENT_BINARY_DIR}/deps/datasketches/")
 set(DATASKETCHES_INSTALL_DIR "${EXTERN_DIR}/datasketches/")
 
 
-execute_process(
-    COMMAND ${CMAKE_COMMAND}
-        -S ${DATASKETCHES_SOURCES_DIR}
-        -B ${DATASKETCHES_BUILD_DIR}
-        -DCMAKE_INSTALL_PREFIX=${DATASKETCHES_INSTALL_DIR}
-        -DBUILD_TESTS=OFF
-    RESULT_VARIABLE result
-)
+if (NOT EXISTS "${DATASKETCHES_INSTALL_DIR}/lib/DataSketches/cmake/")
+
+    execute_process(
+        COMMAND ${CMAKE_COMMAND}
+            -S ${DATASKETCHES_SOURCES_DIR}
+            -B ${DATASKETCHES_BUILD_DIR}
+            -DCMAKE_INSTALL_PREFIX=${DATASKETCHES_INSTALL_DIR}
+            -DBUILD_TESTS=OFF
+        RESULT_VARIABLE result
+    )
 
 
-if (NOT result EQUAL 0)
-    message(FATAL_ERROR "Failed to configure Datasketches: ${result}")
-endif()
+    if (NOT result EQUAL 0)
+        message(FATAL_ERROR "Failed to configure Datasketches: ${result}")
+    endif()
 
-execute_process(
-    COMMAND ${CMAKE_COMMAND} --build ${DATASKETCHES_BUILD_DIR} -j 8 --target install
-    RESULT_VARIABLE result
-)
+    execute_process(
+        COMMAND ${CMAKE_COMMAND} --build ${DATASKETCHES_BUILD_DIR} -j 8 --target install
+        RESULT_VARIABLE result
+    )
 
-if (NOT result EQUAL 0)
-    message(FATAL_ERROR "Failed to configure Datasketches: ${result}")
+    if (NOT result EQUAL 0)
+        message(FATAL_ERROR "Failed to configure Datasketches: ${result}")
+    endif()
+
+else()
+    message(STATUS "Apache.datasketches installed at ${DATASKETCHES_INSTALL_DIR}")
 endif()
 
 list(APPEND CMAKE_PREFIX_PATH "${DATASKETCHES_INSTALL_DIR}/lib/DataSketches/cmake/")
